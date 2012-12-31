8418a81c146ab77a3f15d035e8642ffeff57188a
Shomiron Ghose <ronnie.ghose@gmail.com>
2012-12-30 20:53:13 -0500

    init

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return render_template('hello.html')
+
+@app.route('/dino')
+def dinosaur():
+	return render_template('dino.html')
+
+
+#@app.route('/css/<path:fnc>')
+def css_static(fn):
+	return send_from_directory('/css/',fnc)
+
+#@app.route('/static/<path:fns>')
+def static(fn):
+	return send_from_directory('/static/',fns)
+
+#@app.route('/js/<path:fnj>')
+def js_static(fn):
+	return send_from_directory(app.config['JS_STATIC'],fnj)
+
+
+
+@app.route('/temp/<name>')
+def temp(name=None):
+	return render_template('display.html',name=name)
+
+if __name__ == '__main__':
+	# Bind to PORT if defined, otherwise default to 5000
+	port = int(os.environ.get('PORT',5000))
+	app.run(host='0.0.0.0',port=port,debug=True)
